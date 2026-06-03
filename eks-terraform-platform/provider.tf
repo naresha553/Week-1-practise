e90019735dd1da948a786fe355b06b819448338e
Naresh Babu <naresha553@gmail.com>
2026-06-03 14:02:52 -0400

    fix: Refactor Helm provider configuration for consistency

--- eks-terraform-platform/provider.tf
+++ eks-terraform-platform/provider.tf
@@ -9,11 +9,9 @@ provider "kubernetes" {
 }
 
 provider "helm" {
-  kubernetes {
-    host                   = aws_eks_cluster.main.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority.0.data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
-  }
+  host                   = aws_eks_cluster.main.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
 data "aws_eks_cluster_auth" "cluster" {
